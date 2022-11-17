cdc77a1e3ac876b8c4c9db43c862c37d0e67f84f
Matyas Fodor <matyas.fodor@gmail.com>
2022-11-17 11:05:44 +0100

    feat: random graph

--- apps/simple-example/src/App.tsx
+++ apps/simple-example/src/App.tsx
@@ -1,75 +1,118 @@
-import * as wasmForceatlas2 from 'wasm-forceatlas2'
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import * as wasmForceatlas2 from "wasm-forceatlas2";
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 import Graph from "graphology";
 import { SigmaContainer } from "@react-sigma/core";
 
+const getRandom = (seed: number): (() => number) => {
+  return (): number => {
+    var x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x);
+  };
+};
 
-function useDebounce<T, U>(value: T, callback: (a: T) => U, delay?: number): U {
-  const [debouncedValue, setDebouncedValue] = useState<U>(callback(value))
-
-  useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(callback(value)), delay || 500)
-    return () => {
-      clearTimeout(timer)
-    }
-  }, [value, delay])
-  return debouncedValue
+/**
+ * returns a random k element subset of s
+ * @param s list of elements
+ * @param k number of elements to return
+ * @returns subset of s
+ */
+function randomChoose<T>(s: T[], k: number, random: () => number): T[] {
+  var a = [],
+    i = -1,
+    j;
+  while (++i < k) {
+    j = Math.floor(random() * s.length);
+    a.push(s.splice(j, 1)[0]);
+  }
+  return a;
 }
 
+/**
+ * returns the list of all unordered pairs from s
+ * @param s a list of elements
+ * @returns
+ */
+function unorderedPairs<T>(s: T[]): T[][] {
+  var i = -1,
+    a = [],
+    j;
+  while (++i < s.length) {
+    j = i;
+    while (++j < s.length) a.push([s[i], s[j]]);
+  }
+  return a;
+}
+
+const range = (n: number): number[] => [...Array(n).keys()];
+
+type MinimalGraph = {
+  edges: number[][]; nodes: number;
+}
+
+
+/**
+ * get a random set of links of length l between n nodes
+ * @param n number of nodes
+ * @param l number of links
+ * @param random injected random function to seed the algorithm
+ * @returns list of links
+ */
+const getRandomGraph = (n: number, l: number, random: () => number): MinimalGraph => {
+  return {
+    edges: randomChoose(unorderedPairs(range(n)), l, random),
+    nodes: n,
+  }
+};
+
+function useDebounce<T, U>(value: T, callback: (a: T) => U, delay?: number): U {
+  const [debouncedValue, setDebouncedValue] = useState<U>(callback(value));
+
+  useEffect(() => {
+    const timer = setTimeout(
+      () => setDebouncedValue(callback(value)),
+      delay || 500
+    );
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+  return debouncedValue;
+}
 
 function App() {
   const [inputValue, setInputValue] = useState<string>("");
   const [graph, setGraph] = useState<null | Graph>(null);
+  const [seed, setSeed] = useState(0);
+  const [numberOfNodes, setNumberOfNodes] = useState<number>(32);
+  const [numberOfEdges, setNumberOfEdges] = useState<number>(10);
 
-  const params = useDebounce(inputValue, (inputVal): null | {edges: number[][], nodes: number} => {
-    try {
-      const edges: number[][] = JSON.parse(inputVal);
-      let nodes = 0;
-      edges.forEach((pair: number[]) => {
-        if (!Array.isArray(pair) || pair.length !== 2) {
-          throw new Error("Invalid pair");
-        }
-        pair.forEach((number: number) => {
-          if (number > nodes) {
-            nodes = number;
-          }
-        });
-      });
-
-      nodes += 1;
-
-      return {
-        edges,
-        nodes,
-      }
-    } catch (e) {
-      console.log('Error parsing');
-      return null;
-    }
-  });
-
-  useEffect(() => {
-    const graph = new Graph();
-    graph.addNode("A", { x: 0, y: 0, label: "Node A", size: 10 });
-    graph.addNode("B", { x: .5, y: .5, label: "Node B", size: 10 });
-    graph.addEdge("A", "B");
-
-    setGraph(graph);
-  }, []);
-
-  const [response, setResponse] = useState<unknown | null>(null)
-
+  const [params, setParams] = useState<MinimalGraph>(getRandomGraph(numberOfNodes, numberOfEdges, getRandom(seed)))
 
   useEffect(() => {
     if (params) {
       const resp = wasmForceatlas2.generate_layout(params);
-      setResponse(JSON.parse(resp));
+      const parsedResponse: number[][] = JSON.parse(resp);
+
+      const graph = new Graph();
+
+      parsedResponse.forEach((coords, i) => {
+        graph.addNode(i, {
+          x: coords[0],
+          y: coords[1],
+          label: `Node ${i}`,
+          size: 10,
+        });
+      });
+
+      params.edges.forEach(([from, to]) => {
+        graph.addEdge(from, to);
+      });
+
+      setGraph(graph);
     }
-
-  }, [params])
-
+  }, [params]);
 
   return (
     <div className="App">
@@ -83,14 +126,21 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <input onChange={(e) => setInputValue(e.target.value)} value={inputValue}/>
-        <pre>{JSON.stringify(response, null, 2)}</pre>
+        <input
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        />
       </div>
       <div>
-        {graph && <SigmaContainer style={{ height: "500px", width: "500px", position: "relative" }} graph={graph}/>}
+        {graph && (
+          <SigmaContainer
+            style={{ height: "500px", width: "500px", position: "relative" }}
+            graph={graph}
+          />
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
